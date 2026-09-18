a83b98d51d83a95d46474c0546d717ba5f8338f0
Jacopo Dall'Aglio <fingolfin00@gmail.com>
2026-09-18 22:36:53 +0200

    cities.py: add common city locations
    Signed-off-by: Jacopo Dall'Aglio <fingolfin00@gmail.com>

---
+++ cities.py
@@ -0,0 +1,83 @@
+CITY_LOCATIONS = {
+    "newyork": {
+        "lat_range": (41.2, 40.2),
+        "lon_range": (-74.5, -73.3),
+    },
+    "boston": {
+        "lat_range": (42.8, 41.8),
+        "lon_range": (-71.6, -70.4),
+    },
+    "washington_dc": {
+        "lat_range": (39.4, 38.4),
+        "lon_range": (-77.6, -76.4),
+    },
+    "miami": {
+        "lat_range": (26.2, 25.2),
+        "lon_range": (-80.8, -79.6),
+    },
+    "chicago": {
+        "lat_range": (42.3, 41.3),
+        "lon_range": (-88.3, -87.1),
+    },
+    "houston": {
+        "lat_range": (30.3, 29.3),
+        "lon_range": (-96.1, -94.9),
+    },
+    "denver": {
+        "lat_range": (40.3, 39.3),
+        "lon_range": (-105.6, -104.4),
+    },
+    "seattle": {
+        "lat_range": (48.1, 47.1),
+        "lon_range": (-122.9, -121.7),
+    },
+    "sanfrancisco": {
+        "lat_range": (38.3, 37.3),
+        "lon_range": (-122.9, -121.7),
+    },
+    "losangeles": {
+        "lat_range": (34.6, 33.6),
+        "lon_range": (-118.9, -117.7),
+    },
+
+    "phoenix": {
+        "lat_range": (34.0, 33.0),
+        "lon_range": (-112.7, -111.5),
+    },
+    "dallas": {
+        "lat_range": (33.3, 32.3),
+        "lon_range": (-97.3, -96.1),
+    },
+    "atlanta": {
+        "lat_range": (34.3, 33.3),
+        "lon_range": (-84.9, -83.7),
+    },
+    "minneapolis": {
+        "lat_range": (45.5, 44.5),
+        "lon_range": (-93.9, -92.7),
+    },
+    "saltlakecity": {
+        "lat_range": (41.3, 40.3),
+        "lon_range": (-112.5, -111.3),
+    },
+    "portland": {
+        "lat_range": (46.0, 45.0),
+        "lon_range": (-123.2, -122.0),
+    },
+    "neworleans": {
+        "lat_range": (30.5, 29.5),
+        "lon_range": (-90.7, -89.5),
+    },
+    "nashville": {
+        "lat_range": (36.7, 35.7),
+        "lon_range": (-87.3, -86.1),
+    },
+    "kansascity": {
+        "lat_range": (39.6, 38.6),
+        "lon_range": (-95.2, -94.0),
+    },
+    "detroit": {
+        "lat_range": (42.9, 41.9),
+        "lon_range": (-83.7, -82.5),
+    },
+}
